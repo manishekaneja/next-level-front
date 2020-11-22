01582c60e9796a3c016d0a47c38125eedc6a6e8c
Manish Aneja <manishekaneja@gmail.com>
2020-11-22 13:46:25 +0530

    Commiting Saved local changes

--- components/Post/Post.tsx
+++ components/Post/Post.tsx
@@ -1,11 +1,22 @@
 import { Paper, Typography } from "@material-ui/core";
+import Skeleton from "@material-ui/lab/Skeleton";
 import React from "react";
 import classes from "./post.module.css";
 
 const Post: React.FC<{}> = () => {
+  const loading = parseInt((Math.random() * 2).toFixed(0)) % 2;
+  if (loading) {
+    return (
+      <Paper component={"div"} className={classes.root}>
+        <Typography variant="body1">
+          <Skeleton />
+        </Typography>
+      </Paper>
+    );
+  }
   return (
     <Paper className={classes.root}>
-      <Typography>Hi, user how are You.</Typography>
+      <Typography variant="body1">Hi, user how are You.</Typography>
     </Paper>
   );
 };
